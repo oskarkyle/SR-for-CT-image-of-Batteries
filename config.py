02bcfab699a76996ade63d612fcc97e69d5e5a79
Haorui Long <oskarlong730@gmail.com>
2023-08-11 12:56:10 +0200

    update prediction with hydra

--- config.py
+++ config.py
@@ -1,11 +1,39 @@
 import hydra
+import torch
 from omegaconf import DictConfig, OmegaConf
+import lightning as L
 
-@hydra.main(version_base=None, config_path="configs")
-def train_config(cfg: DictConfig) -> None:
-    print(OmegaConf.to_yaml(cfg))
+from source.BaseDataset import BaseDataset
+from source.data_utils import *
+from utilities import *
+from model.unet.ConvUNet import *
+
+@hydra.main(config_path="configs", config_name="pred")
+def main(cfg:DictConfig):
+    print(cfg)
+    print(cfg.dataset)
+
+    DS = BaseDataset(**cfg.dataset)
     
+    subset_indices = cfg.pred.subset_indices
+    batch_size = cfg.pred.batch_size
 
+    pred_DS = torch.utils.data.Subset(DS, subset_indices)
+    pred_DL = DataLoader(pred_DS, batch_size)
+
+    # Load model
+    ckpt_path = cfg.model.ckp_path
+
+    model: L.LightningModule = ConvUNet().restore(ckpt_path)
+    print(is_lightning_module(model))
+
+
+    # Predict
+    trainer = L.Trainer()
+    predictions = trainer.predict(model, pred_DL)
+
+    # Plot
+    plot_img(predictions, pred_DL)
 
 if __name__ == "__main__":
-    train_config()
+    main()
