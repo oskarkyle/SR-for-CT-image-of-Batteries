6210e2d5369d1f0c6d7a6a2114064f8ba0863d64
tte <thorsten.tegetmeyer-kleine@isea.rwth-aachen.de>
2023-08-21 16:33:01 +0200

    fixes in train and changes from tte

--- train.py
+++ train.py
@@ -5,8 +5,19 @@ from lightning.pytorch.loggers import TensorBoardLogger
 from model.unet.ConvUNet import *
 from utilities import *
 
+from clearml import Task
+
 @hydra.main(version_base=None, config_path="configs", config_name="train")
 def main(cfg:DictConfig):
+    ############################################################################################################
+    task = Task.init(project_name="Students",
+                     task_name=f"Super Resolution",
+                     task_type=Task.TaskTypes.training,
+                     reuse_last_task_id=False,
+                     auto_resource_monitoring=False,
+                     auto_connect_frameworks={"pytorch": False} # does not upload all the output models
+                     )
+    ############################################################################################################
 
     DS = BaseDataset(**cfg.dataset)
     train_DL, test_DL = prepare_data.prepare_dataloader(DS, cfg.train_params.batch_size)
@@ -18,9 +29,10 @@ def main(cfg:DictConfig):
     model = ConvUNet()
 
     # Train
-    trainer = L.Trainer(max_epochs=cfg.epochs) #callbacks=[LearningRateMonitor(logging_interval='epoch'), TrainLossPlotter('output')]
-    logger = TensorBoardLogger('outputs', name='SR')
-    trainer.fit(model, train_DL, test_DL, logger=logger)
+    logger = TensorBoardLogger('outputs', name='SR', default_hp_metric=False)
+    trainer = L.Trainer(max_epochs=cfg.epochs, logger=logger) #callbacks=[LearningRateMonitor(logging_interval='epoch'), TrainLossPlotter('output')]
+
+    trainer.fit(model, train_DL, test_DL)
 
     # train_loss = trainer.callback_metrics['train_loss']
     # val_loss = trainer.callback_metrics['val_loss']
