12cbc5c7d1b7c78f9ea8819565c42dd1207f5985
Kyle Long <oskarlong730@gmail.com>
2023-09-11 13:21:03 +0200

    change pred and pred utils

--- pred.py
+++ pred.py
@@ -6,59 +6,156 @@ from lightning import Trainer
 from torch.utils.data.dataloader import default_collate
 from torch.utils.data import Subset
 from PIL import Image
+import kornia
 
 from source.BaseDataset import *
 from utilities import *
 from model.unet.ConvUNet import *
 from train import *
+from image_utils.utils import *
+from preprocess.Preprocessor import *
 
-def custom_collate(batch):
-    """
-    Custom collate function to drop none informative tiles on edge to prevent model params pollution
-    """
-    batch = [item for item in batch if item is not None]
-    return default_collate(batch)
+def setup_test_set(cfg: DictConfig):
+    tiff_dataset = TiffDataset(cfg.dataset)
+    train_set, val_set, test_set = split_dataset(cfg, tiff_dataset)
+    return test_set
+
+def setup_input_label(cfg: DictConfig):
+    test_set = setup_test_set(cfg)
+    input, label = test_set.__getitem__(cfg.pred.slice - 1)
+    return input, label
+
+def splitting(image: torch.Tensor, cfg: DictConfig):
+    image = convert_tensor_to_numpy(image)
+
+    # Get the dimensions of the image
+    height, width = image.shape
+
+    # Define the row and column 
+    row = cfg.dataset.tile_grid
+    col = cfg.dataset.tile_grid
+
+    # Define tile size
+    tile_height = height // col
+    tile_width = width // row   
+
+    # Initialize an empty list to store the crops
+    tiles = []
+
+    # Iterate over the rows and columns
+    for i in range(col):
+        for j in range(row):
+            # Calculate the coordinates for the current crop
+            top = i * tile_height
+            bottom = (i + 1) * tile_height
+            left = j * tile_width
+            right = (j + 1) * tile_width
+
+            # split the image
+            tile = image[top:bottom, left:right]
+            tile = torch.from_numpy(tile)
+            if tile.shape != (cfg.dataset.tile_size, cfg.dataset.tile_size):
+                tile = Preprocessor.resize(tile, (cfg.dataset.tile_size, cfg.dataset.tile_size))
+            
+            tile = tile.unsqueeze(0)
+            
+            # Append the crop to the list
+            tiles.append(tile)
+
+    return tiles
 
 def load_model(cfg: DictConfig):
     path = cfg.dataset.data_root
     os.chdir(path)
     myckpt_path = os.getcwd() + cfg.pred.ckpt_path
     logger.info(f"Attempting to load checkpoint .. \n\tmodel_class: {ConvUNet.__name__}\n\tcheckpoint: {myckpt_path}")
-    model = ConvUNet.load_from_checkpoint(myckpt_path, map_location=torch.device('mps'), ch_mults=cfg.model.ch_mults, n_blocks=cfg.model.n_blocks, n_layers=cfg.model.n_layers)
+    model = ConvUNet.load_from_checkpoint(myckpt_path, map_location=torch.device(cfg.pred.device), ch_mults=cfg.model.ch_mults, n_blocks=cfg.model.n_blocks, n_layers=cfg.model.n_layers)
     logger.success(f"Successfully loaded checkpoint")
 
     return model
 
-def setup_pred_tiles_and_labels(cfg: DictConfig):
-    tiles = []
-    labels = []
-    pred_DS = BaseDataset(**cfg.dataset)
-    subset_indices = list(range(0, cfg.pred.length))
-    pred_DS = Subset(pred_DS, subset_indices)
-
-    for i in range(len(pred_DS)):
-        input, label = pred_DS.__getitem__(i)
-        
-        tiles.append(input)
-        labels.append(label)
-    return tiles, labels
 
 def prediction(model, tile):
-    device = torch.device("mps")
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps")
     input = tile.to(device)
     output = model(input)
     return output
 
+def reassemble(tiles: list, cfg:DictConfig):
+    reassembled_image = np.zeros((cfg.dataset.tile_size * cfg.dataset.tile_grid, cfg.dataset.tile_size * cfg.dataset.tile_grid), dtype=tiles[0].dtype)
+    num_col = cfg.dataset.tile_grid
+    num_row = cfg.dataset.tile_grid
+    for i in range(num_col):
+        for j in range(num_row):
+            y_start = i * cfg.dataset.tile_size
+            y_end = (i + 1) * cfg.dataset.tile_size
+
+            x_start = j * cfg.dataset.tile_size
+            x_end = (j + 1) * cfg.dataset.tile_size
+
+            reassembled_image[y_start:y_end, x_start:x_end] = tiles[i * num_col + j]
+
+    reassembled_image = torch.from_numpy(reassembled_image)
+    reassembled_image = Preprocessor.resize(reassembled_image, (cfg.interpolation.height, cfg.interpolation.width))
+    return reassembled_image
+
+
 def inference(cfg: DictConfig):
     model = load_model(cfg)
-    tiles, labels = setup_pred_tiles_and_labels(cfg)
-    for i in range(len(tiles)):
-        tile = tiles[i].unsqueeze(0)
-        output = prediction(model, tile)
-        tile = tile.cpu().squeeze(0)
-        input = tile.squeeze(0)
-        label = labels[i].squeeze(0)    
-        output = output.squeeze(0).cpu().detach().numpy()
-        pred = output.squeeze(0)
-        output_path = cfg.dataset.data_root + cfg.pred.output_dir + cfg.pred.output_name + f'_{i}.png'
-        save_image(input, pred, label, output_path)
+    model.eval()
+    
+    input, label = setup_input_label(cfg)
+    input_resize = Preprocessor.resize(input, (cfg.dataset.tile_size, cfg.dataset.tile_size))
+
+    inputs = splitting(input, cfg)
+    labels = splitting(label, cfg)
+    inputs_resize = splitting(input_resize, cfg)
+
+    for i in range(len(inputs)):
+        input = inputs[i].unsqueeze(0)
+        label = labels[i].unsqueeze(0)
+        input_resize = inputs_resize[i].unsqueeze(0)
+        output = prediction(model, input)
+        output = output.cpu()
+
+        
+        print(f"tile_{i + 1}_output_shape: {output.shape}")
+
+        images = [input, output, label]
+        titles = ['LR', 'SR', 'Ground Truth']
+        output_path = os.getcwd() + cfg.pred.output_dir + cfg.pred.output_name + f"_slice_" + str(cfg.pred.slice) + f"_{i}.png"
+
+        save_image(images, output_path, titles)
+    
+    #save_image([input_resize, input, label], os.getcwd()+cfg.pred.output_dir+cfg.pred.output_name+'slice'+str(cfg.pred.slice)+'.png', ['LR', 'SR', 'Label'], axis=False)
+
+
+def calc_average_psnr_in_testset(cfg:DictConfig):
+    testset = setup_test_set(cfg)
+    model = load_model(cfg)
+    model.eval()
+    psnr_list = []
+    average_psnr_list = []
+
+    for i in range(testset.__len__()):
+        input, label = testset.__getitem__(i)
+        split_inputs = splitting(input, cfg)
+        split_labels = splitting(label, cfg)
+
+        for j in range(len(split_inputs)):
+            tile = split_inputs[j].unsqueeze(0)
+            label = split_labels[j].unsqueeze(0)
+            output = prediction(model, tile)
+            output = output.cpu()
+            psnr = calc_psnr(output, label)
+            psnr = psnr.item()
+            psnr_list.append(psnr)
+
+        slice_psnr = sum(psnr_list) / len(psnr_list)
+        print(f"第{i+1}张的PSNR: {slice_psnr}")
+        average_psnr_list.append(slice_psnr)
+    
+    average_psnr(average_psnr_list)
+
+
+
--- utilities.py
+++ utilities.py
@@ -6,8 +6,11 @@ import cv2
 from source.BaseDataset import *
 from image_utils.utils import *
 import numpy as np
+import os 
+from torch.utils.data import Dataset
 
-def split_dataset(cfg: DictConfig, dataset: BaseDataset):
+
+def split_dataset(cfg: DictConfig, dataset):
     split_ratios = cfg.split_ratios
     train_ratio = split_ratios.train
     val_ratio = split_ratios.val 
@@ -21,27 +24,18 @@ def split_dataset(cfg: DictConfig, dataset: BaseDataset):
     train_dataset,validation_dataset,test_dataset = random_split(dataset,[train_size,val_size,test_size])
     return train_dataset,validation_dataset,test_dataset
 
-def save_image(input: torch.Tensor, pred: torch.Tensor, label: torch.Tensor, output_path):
-    input = convert_tensor_to_numpy(input)
-    pred = convert_tensor_to_numpy(pred)
-    label = convert_tensor_to_numpy(label)
+def save_image(image_list: list, output_path, title_list: list = None, axis: bool = False):
+    fig, ax = plt.subplots(1, len(image_list), figsize=(12, 5))
+    for i in range(len(image_list)):
+        ax[i].imshow(convert_tensor_to_numpy(image_list[i]), cmap='gray')
+        if not axis:
+            ax[i].axis('off')
+        if title_list:
+            title = ax[i].set_title(title_list[i])
+            title.set_fontsize(12)
 
-    fig, ax = plt.subplots(1, 3, figsize=(12, 5))
-    ax[0].imshow(input, cmap='gray')
-    title = ax[0].set_title('Input')
-    title.set_fontsize(20)
-
-    ax[1].imshow(pred, cmap='gray')
-    title = ax[1].set_title('Prediction')
-    title.set_fontsize(20)
-
-    ax[2].imshow(label, cmap='gray')
-    title = ax[2].set_title('Label')
-    title.set_fontsize(20)
-
-    
     plt.tight_layout()
-    plt.savefig(output_path)
+    plt.savefig(output_path, bbox_inches='tight')
 
 def save_image_in_RGB(input: torch.Tensor, pred: torch.Tensor, label: torch.Tensor, output_path: str):
     input = convert_tensor_to_numpy(input)
@@ -89,3 +83,128 @@ def calc_total_page_of_one_file(cfg: DictConfig, dataset: BaseDataset):
     total_page = dataset_size // total_tiles_in_one_page
     total_page_of_one_file = total_page // total_files
     return total_page_of_one_file
+
+class TiffDataset(Dataset):
+    def __init__(self, 
+                 cfg: DictConfig,
+                 ):   
+        self.data_root = cfg.data_root
+        self.dataset_dir = cfg.dataset_dir
+        self.task = cfg.task
+        self.cfg = cfg
+        self.file_list = self.load_file_paths_from_dir(self.data_root, self.dataset_dir)
+        self.process_cfg = cfg.preprocess_cfg
+        self.preprocess = self.get_preprocessor(self.process_cfg)
+        self.transform_cfg = cfg.transforms_cfg
+        self.transform = self.get_transform(self.transform_cfg)
+
+    @staticmethod
+    def load_file_paths_from_dir(data_root: str, dataset_dir: List[str], recursive: bool=True) -> List[Path]:
+        """
+        Recursively loads all file paths from the given directory or directories.
+
+        Args:
+            data_root (str): The root directory to which the dataset directory paths are relative.
+            dataset_dir (List[str]): A list of directory paths to load file paths from.
+            recursive (bool, optional): Whether to recursively load file paths from all subdirectories. Defaults to True.
+
+        Returns:
+            List[Path]: A sorted list of all file paths found in the given directories.
+
+        Raises:
+            FileNotFoundError: If any of the given directories are not found.
+
+        """
+        file_list = []
+        for dir in dataset_dir:
+            dir = Path(data_root+dir)
+            try:
+                for dirname in dir.iterdir():
+                    if dirname.is_file():
+                        file_list.append(dirname)
+                    elif dirname.is_dir() and recursive:
+                        file_list += BaseDataset.load_file_paths_from_dir('', [dirname.as_posix()], recursive=recursive)
+            except FileNotFoundError as ex:
+                logger.error(ex)
+                logger.error(f"skipping dir: {dir}")
+
+        return natsorted(file_list)
+    
+    @staticmethod
+    def get_preprocessor(preprocess_cfg: DictConfig) -> Optional[Preprocessor]:
+        """
+        Returns a Preprocessor object based on the provided preprocessing configuration.
+
+        Args:
+            preprocess_cfg (DictConfig): A configuration object that specifies the details of the preprocessing pipeline.
+
+        Returns:
+            Preprocessor: A Preprocessor object that can be used to preprocess the data.
+
+        Notes:
+            This method creates a Preprocessor object based on the provided configuration.
+            If no configuration is provided (i.e., if preprocess_cfg is None), None is returned.
+        """
+        
+        if preprocess_cfg is None:
+            return None
+        return Preprocessor(preprocess_cfg)
+    
+    @staticmethod
+    def get_transform(transforms_cfg: DictConfig) -> Optional[Transforms]:
+        """
+        Returns a Transform object based on the provided configuration.
+
+        Args:
+            transforms_cfg (DictConfig): The configuration for the transforms.
+
+        Returns:
+            Transforms: The Transform object.
+
+        Notes:
+            This method creates a Transforms object based on the provided configuration.
+            If no configuration is provided (i.e., if transforms_cfg is None), None is returned.
+        """
+        if transforms_cfg is None: return None
+        cfg = transforms_cfg.copy()
+        return Transforms(cfg)
+    
+    def load_page_from_tiff_file(self):
+        file_list = self.file_list
+        pages_tensor = []
+        for i in range(len(file_list)):
+            tiff_file = file_list[i]
+            with tifffile.TiffFile(tiff_file) as tif:
+                pages_number = len(tif.pages)
+                for i in range(pages_number):
+                    page = tif.pages[i]
+                    page_array = page.asarray()
+                    page_tensor = torch.from_numpy(page_array)
+                    pages_tensor.append(page_tensor)
+                
+                return pages_tensor
+    
+    def __len__(self):
+        pages_tensor = self.load_page_from_tiff_file()
+        return len(pages_tensor)
+    
+    def __getitem__(self, index):
+        pages_tensor = self.load_page_from_tiff_file()
+        raw_data_tensor = pages_tensor[index]
+        raw_data_float_tensor = torch.tensor(raw_data_tensor.clone().detach(), dtype=torch.float32)
+        data = raw_data_float_tensor.unsqueeze(0)
+        label = data
+
+        #Preprocess
+        if self.preprocess:
+            data, label = self.preprocess(data, label)
+
+        # Transform
+        if self.transform:
+            data, label = self.transform(data, label)
+
+        return data, label
+    
+def average_psnr(psnr_list: list):
+    psnr_average = sum(psnr_list) / len(psnr_list)
+    print(psnr_average)
